73dabd9a848028d74b00ffe24ccaf5edbef039d5
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-12 15:35:45 +0700

    Add subtract function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-export function add(a, b) {
+function add(a, b) {
   return a + b;
 }
+
+function subtract(a, b) {
+  return a - b;
+}
+
+export { add, subtract };
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,18 @@
 import { expect } from 'chai';
-import { add } from '../src/index';
+import { add, subtract } from '../src/index';
 
-describe('add', () => {
+describe('Basic operations', () => {
   it('should add a and b correctly', () => {
     const a = 10;
     const b = 20;
     const sum = add(a, b);
     expect(sum).to.equal(30);
+  });
+
+  it('should subtract a and b correctly', () => {
+    const a = 30;
+    const b = 20;
+    const result = subtract(a, b);
+    expect(result).to.equal(10);
   });
 });
