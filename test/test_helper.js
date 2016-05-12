ddff5dcbe099fde1c47b7c4e18dc3ab8e4f718b9
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-12 16:12:48 +0700

    Add more package to test_helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,3 +1,12 @@
+import _$ from 'jquery';
+import jsdom from 'jsdom';
 import chai, { expect } from 'chai';
+import chaiJquery from 'chai-jquery';
+
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+const $ = _$(window);
+
+chaiJquery(chai, chai.util, $);
 
 export { expect };
