da527a97b526d4c3c2525fc02bec05ca722f3c46
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-16 16:42:26 +0700

    Add TodoList test

---
+++ test/components/todo_list.test.js
@@ -0,0 +1,18 @@
+import { renderComponent, expect } from '../test_helper';
+import TodoList from '../../src/components/TodoList';
+
+describe('TodoList', () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(TodoList);
+  })
+
+  it('has a correct class', () => {
+    expect(component).to.have.class('todo-list');
+  });
+
+  it('contains a <ul> tag', () => {
+    expect(component.find('ul')).to.exist;
+  });
+});
