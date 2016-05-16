7e75a95184c52b1551c3fc0c712870007e3b4918
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-16 17:54:05 +0700

    Clear input box after submit

--- src/components/TodoInputBox.js
+++ src/components/TodoInputBox.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../redux/actions/todos';
 
 class TodoInputBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { todo: '' };
+  }
+
+  handleChange(event) {
+    this.setState({ todo: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.addTodo(this.state.todo);
+    this.setState({ todo: '' });
+  }
+
   render() {
     return (
-      <div className="todo-input-box">
-        <input />
+      <form onSubmit={this.handleSubmit.bind(this)} className="todo-input-box">
+        <h4>Add Todo</h4>
+        <input
+          onChange={this.handleChange.bind(this)}
+          value={this.state.todo} />
         <button>Add</button>
-      </div>
+      </form>
     );
   }
 }
 
-export default TodoInputBox;
+export default connect(null, actions)(TodoInputBox);
--- test/components/todo_input_box.test.js
+++ test/components/todo_input_box.test.js
@@ -19,4 +19,28 @@ describe('TodoInputBox', () => {
   it('has a button', () => {
     expect(component.find('button')).to.exist;
   });
+
+  describe('add a new todo', () => {
+    beforeEach(() => {
+      component.find('input').simulate('change', 'Drink Coffee');
+    });
+
+    it('shows the text in input box', () => {
+      expect(component.find('input')).to.have.value('Drink Coffee');
+    });
+
+    describe('after click the add button', () => {
+      beforeEach(() => {
+        component.simulate('submit');
+      });
+
+      it('clears the input box when clicking the add button', () => {
+        expect(component.find('input')).to.have.value('');
+      });
+
+      it('adds a new todo into the todo list', () => {
+        expect(component.find('li')).to.contain('Drink Coffee');
+      })
+    });
+  });
 });
