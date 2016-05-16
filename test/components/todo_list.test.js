3a94e2bb5043c6f7dffa2002093e960e316c40e3
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-16 22:23:04 +0700

    Test TodoList to show each todo

--- test/components/todo_list.test.js
+++ test/components/todo_list.test.js
@@ -5,7 +5,13 @@ describe('TodoList', () => {
   let component;
 
   beforeEach(() => {
-    component = renderComponent(TodoList);
+    const props = {
+      todos: [
+        { text: 'Drink Coffee', status: 'incompleted' },
+        { text: 'Read a book', status: 'completed' }
+      ]
+    };
+    component = renderComponent(TodoList, null, props);
   })
 
   it('has a correct class', () => {
@@ -14,5 +20,14 @@ describe('TodoList', () => {
 
   it('contains a <ul> tag', () => {
     expect(component.is('ul')).to.be.true;
+  });
+
+  it('shows an <li> tag for each todo', () => {
+    expect(component.find('li').length).to.equal(2);
+  });
+
+  it('shows each todo that is provided', () => {
+    expect(component).to.contain('Drink Coffee');
+    expect(component).to.contain('Read a book');
   });
 });
