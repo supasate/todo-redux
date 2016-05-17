59b440b5e11ee9fa851e28c3245096a092f9888d
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-17 23:51:24 +0700

    Refactor code and test the state reference to be differen

--- test/reducers/todos.test.js
+++ test/reducers/todos.test.js
@@ -3,89 +3,123 @@ import { reducer as todoReducer } from '../../src/reducers/todos';
 import * as types from '../../src/actions/types';
 
 describe('Todo Reducer', () => {
+  let curState;
+
+  beforeEach(() => {
+    curState = [
+      { id: 1, text: 'Joggin', status: 'incomplete' },
+      { id: 2, text: 'Drink Coffee', status: 'completed' },
+      { id: 3, text: 'Watch Movie', status: 'incomplete' }
+    ];
+  });
+
   it('should return correct initial state', () => {
     const expectedState = [];
     expect(todoReducer(undefined, {})).to.deep.equal(expectedState);
   });
 
-  it('should add new todo when receiving ADD_TODO action', () => {
-    const curState = [
-        { id: 1, text: 'Buy Coffee', status: 'completed' },
-        { id: 2, text: 'Jogging', status: 'incomplete' },
-    ];
+  describe('add', () => {
+    let resultState;
 
-    const action = {
-      type: types.ADD_TODO,
-      id: 3,
-      payload: 'Read Book'
-    };
+    beforeEach(() => {
+      const action = {
+        type: types.ADD_TODO,
+        id: 4,
+        payload: 'Read Book'
+      };
+      resultState = todoReducer(curState, action);
+    });
 
-    const expectedState = [
-        { id: 1, text: 'Buy Coffee', status: 'completed' },
-        { id: 2, text: 'Jogging', status: 'incomplete' },
-        { id: 3, text: 'Read Book', status: 'incomplete' },
-    ];
-    expect(todoReducer(curState, action)).to.deep.equal(expectedState);
+    it('should add new todo when receiving ADD_TODO action', () => {
+      const expectedState = [
+        { id: 1, text: 'Joggin', status: 'incomplete' },
+        { id: 2, text: 'Drink Coffee', status: 'completed' },
+        { id: 3, text: 'Watch Movie', status: 'incomplete' },
+        { id: 4, text: 'Read Book', status: 'incomplete' }
+      ];
+      expect(resultState).to.deep.equal(expectedState);
+    });
+
+    it('should not return the same reference of state', () => {
+      expect(resultState).to.not.equal(curState);
+    });
   });
 
-  it('should update old msg to new msg when receiving UPDATE_TODO action', () => {
-    const curState = [
-        { id: 1, text: 'Jogging', status: 'incomplete' },
-        { id: 2, text: 'Buy Coffee', status: 'completed' }
+  describe('update', () => {
+    let resultState;
+
+    beforeEach(() => {
+      const action = {
+        type: types.UPDATE_TODO,
+        id: 2,
+        payload: 'Drink Juice'
+      };
+      resultState = todoReducer(curState, action);
+    });
+
+    it('should update old msg to new msg when receiving UPDATE_TODO action', () => {
+      const expectedState = [
+        { id: 1, text: 'Joggin', status: 'incomplete' },
+        { id: 2, text: 'Drink Juice', status: 'completed' },
+        { id: 3, text: 'Watch Movie', status: 'incomplete' }
+      ];
+      expect(resultState).to.deep.equal(expectedState);
+    });
+
+    it('should not return the same reference of state', () => {
+      expect(resultState).to.not.equal(curState);
+    });
+  });
+
+  describe('delete', () => {
+    let resultState;
+
+    beforeEach(() => {
+      const action = {
+        type: types.DELETE_TODO,
+        id: 2
+      };
+
+      resultState = todoReducer(curState, action);
+    });
+
+    it('should delete a todo with given id', () => {
+      const expectedState = [
+        { id: 1, text: 'Joggin', status: 'incomplete' },
+        { id: 3, text: 'Watch Movie', status: 'incomplete' }
       ];
 
-    const action = {
-      type: types.UPDATE_TODO,
-      id: 2,
-      payload: 'Drink Coffee'
-    };
+      expect(resultState).to.deep.equal(expectedState);
+    });
 
-    const expectedState = [
-        { id: 1, text: 'Jogging', status: 'incomplete' },
-        { id: 2, text: 'Drink Coffee', status: 'completed' }
-    ];
-
-    expect(todoReducer(curState, action)).to.deep.equal(expectedState);
+    it('should not return the same reference of state', () => {
+      expect(resultState).to.not.equal(curState);
+    });
   });
 
-  it('should delete a todo with given id', () => {
-    const curState = [
-      { id: 1, text: 'Joggin', status: 'incomplete' },
-      { id: 2, text: 'Drink Coffee', status: 'completed' },
-      { id: 3, text: 'Watch Movie', status: 'incomplete' }
-    ];
+  describe('toggle', () => {
+    let resultState;
 
-    const action = {
-      type: types.DELETE_TODO,
-      id: 2
-    };
+    beforeEach(() => {
+      const action = {
+        type: types.TOGGLE_TODO,
+        id: 1
+      };
+      resultState = todoReducer(curState, action);
+    });
 
-    const expectedState = [
-      { id: 1, text: 'Joggin', status: 'incomplete' },
-      { id: 3, text: 'Watch Movie', status: 'incomplete' }
-    ];
+    it('should toggle a todo status with given id', () => {
+      const expectedState = [
+        { id: 1, text: 'Joggin', status: 'completed' },
+        { id: 2, text: 'Drink Coffee', status: 'completed' },
+        { id: 3, text: 'Watch Movie', status: 'incomplete' }
+      ];
 
-    expect(todoReducer(curState, action)).to.deep.equal(expectedState);
-  });
+      expect(resultState).to.deep.equal(expectedState);
+    });
 
-  it('should toggle a todo status with given id', () => {
-    const curState = [
-      { id: 1, text: 'Joggin', status: 'incomplete' },
-      { id: 2, text: 'Drink Coffee', status: 'completed' },
-      { id: 3, text: 'Watch Movie', status: 'incomplete' }
-    ];
-
-    const action = {
-      type: types.TOGGLE_TODO,
-      id: 1
-    };
-
-    const expectedState = [
-      { id: 1, text: 'Joggin', status: 'completed' },
-      { id: 2, text: 'Drink Coffee', status: 'completed' },
-      { id: 3, text: 'Watch Movie', status: 'incomplete' }
-    ];
-
-    expect(todoReducer(curState, action)).to.deep.equal(expectedState);
+    it('should not return the same reference of state', () => {
+      expect(resultState).to.not.equal(curState);
+    });
   });
 });
